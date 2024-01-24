a1d87613adcaa2d6f159864786907be8c46d3fb5
surajvast1 <shrivastava.is21@bmsce.ac.in>
2024-01-25 00:20:52 +0530

    aas

--- test/index.js
+++ test/index.js
@@ -1,11 +1,4 @@
-import React from 'react'
-
-const index = () => {
-  return (
-    <div>
-      sss
-    </div>
-  )
-}
-
-export default index
+hsedbdkcbjd
+CDATASectiondsc
+DynamicsCompressorNodec
+dzz
