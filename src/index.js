65dfd564a466d395eafc28d7c925b8caf1c62fef
surajvast1 <shrivastava.is21@bmsce.ac.in>
2024-01-24 23:01:33 +0530

    asasaS

--- src/index.js
+++ src/index.js
@@ -1,4 +1,31 @@
 hello ahodjkc
 satisfiess
 satisfiesss
-satisfiess
+satisfiess\a\a\
+try{
+
+    }
+    catch(err)
+    {
+        console.log(err);
+        res.status(500).send({
+            success: false,
+            message: "try{
+            
+                }
+                catch(err)
+                {
+                    console.log(err);
+                    res.status(500).send({
+                        success: false,
+                        message: "a
+                        
+                        ",
+                        error: err
+                    })
+            
+                }",
+            error: err
+        })
+
+    }
