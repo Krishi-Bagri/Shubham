14b35773162ee0d83dcb46d7949fb6d63ed010bd
Shubham Srivastava <99933579+surajvast1@users.noreply.github.com>
2024-01-25 00:44:15 +0530

    Add files via upload

---
+++ content/en/meshmap/hello/making.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const making = () => {
+  return (
+    <div>
+      sdsd
+      sdsdd
+      sdsdsd
+      sdsdsdss
+      s
+      sdsdsdsd
+    </div>
+  )
+}
+
+export default making
