2efd08fc0934518c695130e548c2d516e4522e32
surajvast1 <shrivastava.is21@bmsce.ac.in>
2024-01-27 13:59:28 +0530

    asaSsaaaaaa
    Signed-off-by: surajvast1 <shrivastava.is21@bmsce.ac.in>

--- docs/dsdsd.cpp
+++ docs/dsdsd.cpp
@@ -1,4 +1,24 @@
-dsdss
-shorts
-shorts
-s
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+
+void solution()
+{
+     
+}
+signed main()
+{    
+    int ts;
+    cin >> ts;
+    
+    while(ts--)
+     {
+        solution();
+        cout<<"\n";
+     }
+   return 0;
+}  
+ 
+
+
